7cdd922b6aeff0905d55cb016715461d96926912
Francisco Bustamante <chicolucio@gmail.com>
2021-04-20 15:55:00 -0300

    Segunda versao. Trabalha com unidades.

--- exemplo_16_2.py
+++ exemplo_16_2.py
@@ -1,12 +1,17 @@
 import numpy as np
+import pint
+
+ureg = pint.UnitRegistry()
+Q_ = ureg.Quantity
 
 
-GAS_CONSTANT = 0.082057366080960
+GAS_CONSTANT = Q_(0.082057366080960, 'atm * l / (mol * K)')
 
 
+@ureg.wraps('liter/mol', ('atm * dm**6 / mol**2', 'dm**3 / mol', 'K', 'atm'))
 def van_der_waals(a, b, temperatura, pressao):
     coef_V3 = 1
-    coef_V2 = -(b + GAS_CONSTANT * temperatura / pressao)
+    coef_V2 = -(b + GAS_CONSTANT.magnitude * temperatura / pressao)
     coef_V1 = a / pressao
     coef_V0 = - a * b / pressao
     poly = np.polynomial.Polynomial((coef_V0, coef_V1, coef_V2, coef_V3))
--- test_exemplo_16_2.py
+++ test_exemplo_16_2.py
@@ -1,6 +1,33 @@
-from exemplo_16_2 import van_der_waals
+from exemplo_16_2 import van_der_waals, Q_
 import numpy as np
+# import pint
+
+# ureg = pint.UnitRegistry()
+# Q_ = ureg.Quantity
 
 
 def test_valores_exemplo():
-    assert np.isclose(van_der_waals(5.5088, 0.065144, 300, 200), 0.096, atol=0.001)
+    a = Q_(5.5088, 'atm * dm**6 / mol**2')
+    b = Q_(0.065144, 'dm**3 / mol')
+    temperatura = Q_(300, 'K')
+    pressao = Q_(200, 'atm')
+    resposta_esperada = Q_(0.096, 'l / mol')
+    assert np.isclose(van_der_waals(a, b, temperatura, pressao), resposta_esperada, atol=0.001)
+
+
+def test_valores_exemplo_a_unidade_a_bar():
+    a = Q_(5.5818, 'bar * dm**6 / mol**2')
+    b = Q_(0.065144, 'dm**3 / mol')
+    temperatura = Q_(300, 'K')
+    pressao = Q_(200, 'atm')
+    resposta_esperada = Q_(0.096, 'l / mol')
+    assert np.isclose(van_der_waals(a, b, temperatura, pressao), resposta_esperada, atol=0.001)
+
+
+def test_valores_exemplo_a_unidade_pressao_pascal():
+    a = Q_(5.5818, 'bar * dm**6 / mol**2')
+    b = Q_(0.065144, 'dm**3 / mol')
+    temperatura = Q_(300, 'K')
+    pressao = Q_(2.0265E7, 'Pa')
+    resposta_esperada = Q_(0.096, 'l / mol')
+    assert np.isclose(van_der_waals(a, b, temperatura, pressao), resposta_esperada, atol=0.001)
